375f3f4e8c23d394e82be1f99f5312a2792b2a93
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-11-17 20:38:58 -0500

    guess-who: add bottom Empezar de nuevo

--- guess-who/gameObject.js
+++ guess-who/gameObject.js
@@ -2,50 +2,48 @@ const random = (max) => {
     return Math.floor(Math.random() * max);
 }
 
-class Game {
-    constructor(){
-        this.characters = generatedCharacters;
-        this.selectedCharacter = null;
-        this.handlerCharacters();
-    }
-
-    closeCharacter(event){
+const Game = {
+  
+    "characters" : generatedCharacters,
+    "selectedCharacter" : null,
+    "randomCharacterIndex" : random(this.characters.length),
+    "closeCharacter" : (event) =>{
         event.target.parentNode.classList.add('hide');
-    }
+    },
 
-    openCharacter(event){
+    "openCharacter" : (event)=>{
         event.target.parentNode.classList.remove('hide')
-    }
+    },
 
-    handlerCharacters(){
+    "handlerCharacters" : ()=>{
         document.querySelectorAll('.character').forEach((character, index)=>{            
             character.id = this.characters[index].name;            
             character.firstChild.addEventListener('click', this.closeCharacter);
             character.lastChild.addEventListener('click', this.openCharacter);
         });
-    }
+    },
 
-    createSelectedCharacterCard(){
+    "createSelectedCharacterCard" : ()=>{
         const character = document.getElementById(this.selectedCharacter.name);
         const position = window.getComputedStyle(character)['background-position'];
 
         document.getElementById('selected').style.backgroundPosition = position;
-        // console.log(position);
-    }
+        console.log(position);
+    },
 
-    selectRandomCharacter(){
-        const randomCharacterIndex = random(this.characters.length);
-        this.selectedCharacter = this.characters[randomCharacterIndex];
-    }
-    removeHideClass(){
-        
+    "selectRandomCharacter" : ()=>{
+        this.handlerCharacters();
+        this.selectedCharacter = this.characters[this.randomCharacterIndex];
+        this.createSelectedCharacterCard();
+    },
+
+    "removeHideClass" : ()=>{
         document.querySelectorAll('.hide').forEach((element)=>{
-            element.classList.remove('hide');
+            element.classList.remove('.hide');
         });
-    }
-    start(){
+    },
+    "start" : ()=>{
         this.removeHideClass();
         this.selectRandomCharacter();
-        this.createSelectedCharacterCard();
-    }
+    },
 }
