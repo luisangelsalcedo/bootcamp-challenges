8634580159686ce09609600c0527af6c3c50db7b
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2021-11-26 11:55:16 -0500

    pokedex: correccion css

--- pokedex/poke.js
+++ pokedex/poke.js
@@ -1,13 +1,13 @@
 
-const API = 'https://pokeapi.co/api/v2/pokemon';
-const countPoke     = 150;
+const API           = 'https://pokeapi.co/api/v2/pokemon';
+const count         = 150;
 const pokemones     = document.getElementById('pokemones');
 const info          = document.getElementById('info');
 const infoContainer = document.querySelector('.info-container');
 
 
 const init = ()=>{
-    fetch(`${API}?offset=0&limit=${countPoke}`)
+    fetch(`${API}?offset=0&limit=${count}`)
         .then(response => response.json())
         .then(pokemonJson => createPokeContainer(pokemonJson))
 }
@@ -17,142 +17,137 @@ const createPokeContainer = pokemonJson => {
     const pokeList = pokemonJson.results;
     // console.log(pokemonJson.results);
 
-    pokeList.forEach((el, i) => {
-        fetch(el.url)
+    pokeList.forEach(({name, url}, i) => {
+
+        fetch(url)
             .then(response => response.json())
-            .then(pokemon => pokemonHTML(pokemon))
-            // console.log(el.url);
+            .then(pokemon => insertPokemon(pokemon))
 
         const pokeDiv = document.createElement('div');
-        pokeDiv.classList.add('pokemon', `poke${i + 1}`);
+        pokeDiv.classList.add('pokemon');
+        pokeDiv.id = i + 1;
         
         const pokeName = document.createElement('p');
-        pokeName.innerText = el.name;
+        pokeName.innerText = name;
 
         pokeDiv.append(pokeName);
         pokemones.append(pokeDiv);
-
     });
 }
 
-const pokemonHTML = (pokemon)=>{
-    const pokeDiv = document.querySelector(`.poke${pokemon.id}`);
-    // console.log(pokemon);
+const insertPokemon = ({id, sprites, species})=>{
+
+    const pokeDiv = document.getElementById(id);
+
+    // imagen
+    const pokeimg = document.createElement('img');
+    pokeimg.setAttribute('src', sprites.front_default);
+
+    // especie / color
+    const pokeSpeciesURL = species.url;
+
+    fetch(pokeSpeciesURL).then(response => response.json()).then(species =>{
+        
+        pokeDiv.setAttribute('data-color', species.color.name);
+        pokeDiv.addEventListener('click', (event) => pokeAlertInfo(event.currentTarget.id));
+        pokeDiv.prepend(pokeimg);
+        
+    });    
+}
+
+const pokeAlertInfo = pokemonID => {
+
+    //get pokemon
+    const pokeURL = `${API}/${pokemonID}`;
+    fetch(pokeURL).then(response => response.json()).then(pokemon => getDataPokemon(pokemon));
+    
+    const nextID = (parseInt(pokemonID) >= count) ? 1 : parseInt(pokemonID) + 1;
+    const prevID = (parseInt(pokemonID) <= 1) ? count : parseInt(pokemonID) - 1;
+
+    //btn next
+    const btnNext = document.createElement('a');
+    btnNext.innerHTML = '<i class="fa fa-chevron-right fa-2x" aria-hidden="true"></i>';
+    btnNext.classList.add('btn-next');
+    btnNext.addEventListener('click', () => pokeAlertInfo(nextID));
+
+    //btn prev
+    const btnPrev = document.createElement('a');
+    btnPrev.innerHTML = '<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>';
+    btnPrev.classList.add('btn-prev');
+    btnPrev.addEventListener('click', () => pokeAlertInfo(prevID));
+
+    //btn cerrar
+    const btnClose = document.createElement('a');
+    btnClose.innerHTML = '<i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>';
+    btnClose.classList.add('btn-close');
+    btnClose.addEventListener('click', () => cerrarModal());
+
+    //container
+    const container = document.createElement('div');
+    container.classList.add('container');
+
+    infoContainer.setAttribute('id', `poke${pokemonID}`);
+    infoContainer.classList.add('active');
+    info.innerText = '';
+    info.prepend(btnNext, btnPrev, btnClose, container);
+    
+}
+
+const getDataPokemon = ({ name, sprites, abilities, species, types, moves })=>{
+
+    //Habilidad ES
+    // const pokeAbilitiURL = pokemon.abilities[0].ability.url;
+    // fetch(pokeAbilitiURL).then(response => response.json()).then(abiliti => {
+    //     const abilitiName = (abiliti.names.find(element => element.language.name === 'es').name);
+    //     const abilitiEfect = (abiliti.flavor_text_entries.find(element => element.language.name === 'es').flavor_text);
+    //     const abilitiResponse = `<h3>Habilidad</h3> ${abilitiName}: ${abilitiEfect}`;
+    // });
 
     //imagen
-    const pokeimg = document.createElement('img');
-    pokeimg.setAttribute('src', pokemon.sprites.front_default);
+    const pokeIMG = sprites.other.dream_world.front_default;
+    const pokeimgFront = sprites.front_default;
+    const pokeimgBack = sprites.back_default;
+
+    //Habilidades
+    const pokeAbility = abilities.map(a => a.ability.name).join(', ');
+
+    //movimientos
+    const pokeMoves = moves.map(m => m.move.name);
+    const pokeMovesResumen = (pokeMoves.length > 10) ? pokeMoves.slice(0, 10).join(', ') + '...' : pokeMoves.join(', ');
 
     //especie / color
-    const pokeSpeciesURL = pokemon.species.url;
-    fetch(pokeSpeciesURL).then(response => response.json()).then(species =>{
-        pokeDiv.setAttribute('data-color', species.color.name);
+    const pokeSpeciesURL = species.url;
+    fetch(pokeSpeciesURL).then(response => response.json()).then(s => {
+        infoContainer.setAttribute('data-color', s.color.name);
     });
+
+    //tipo de pokemon
+    const pokeTypes = types.map(t => t.type.name);
+    let pokeTypesHTML = '';
+    pokeTypes.forEach(tipo =>  pokeTypesHTML+=`<span>${tipo}</span>`);
     
-    pokeDiv.prepend(pokeimg);
-    pokeDiv.addEventListener('click', (event)=>{
-        
-        
-        pokeAlertInfo(pokemon.id);
-    })
-    
-}
-
-const pokeAlertInfo = (pokemonID)=>{
-
-
-    const nextID = (parseInt(pokemonID) >= countPoke) ? 1 : parseInt(pokemonID) + 1;
-    const prevID = (parseInt(pokemonID) <= 1) ? countPoke : parseInt(pokemonID) - 1;
-
-    //pokemon
-    const pokeURL = `${API}/${pokemonID}`;
-    fetch(pokeURL).then(response => response.json()).then(pokemon => {
-        //imagen
-        const pokeIMG = pokemon.sprites.other.dream_world.front_default;
-        const pokeimgFront = pokemon.sprites.front_default;
-        const pokeimgBack = pokemon.sprites.back_default;
-
-        //Habilidad
-        // const pokeAbilitiURL = pokemon.abilities[0].ability.url;
-        // fetch(pokeAbilitiURL).then(response => response.json()).then(abiliti => {
-        //     const abilitiName = (abiliti.names.find(element => element.language.name === 'es').name);
-        //     const abilitiEfect = (abiliti.flavor_text_entries.find(element => element.language.name === 'es').flavor_text);
-        //     const abilitiResponse = `<h3>Habilidad</h3> ${abilitiName}: ${abilitiEfect}`;
-        // });
-
-        const pokeAbility = pokemon.abilities.map(ablity => ablity.ability.name);
-
-        //especie / color
-        const pokeSpeciesURL = pokemon.species.url;
-        fetch(pokeSpeciesURL).then(response => response.json()).then(species => {
-            infoContainer.setAttribute('data-color', species.color.name);
-        });
-
-        //tipo de pokemon
-        const pokeTypes = pokemon.types.map(t => t.type.name);
-        let pokeTypesHTML = '';
-        pokeTypes.forEach(tipo => {
-            pokeTypesHTML += `<span>${tipo}</span>`;
-        });
-
-        //movimientos
-        const pokeMoves = pokemon.moves.map(m => m.move.name);
-        const pokeMovesResumen = (pokeMoves.length > 10) ? pokeMoves.slice(0, 10).join(', ') + '...' : pokeMoves.join(', ');
-
-        info.innerHTML = `
-            <div>
-                <div><h1>${pokemon.name}<small>${pokeTypesHTML}</small></h1></div>
+    info.lastChild.innerHTML = `
+        <div>
+            <div><h1>${name}<small>${pokeTypesHTML}</small></h1></div>
+            <div class="images">
                 <div class="img">
-                    <img src="${pokeIMG}" alt="${pokemon.name}">
+                    <img src="${pokeIMG}" alt="${name}">
                 </div>
                 <div class="imgs">
-                    <div><img src="${pokeimgFront}" alt="${pokemon.name} (vista de frente)"></div>
-                    <div><img src="${pokeimgBack}" alt="${pokemon.name} (vista de trasera)"></div>
+                    <div><img src="${pokeimgFront}" alt="${name} (vista de frente)"></div>
+                    <div><img src="${pokeimgBack}" alt="${name} (vista de trasera)"></div>
                 </div>
-                <div class="info">
-                    <div><h3>Abilities</h3> ${pokeAbility.join(', ')}</div>
-                    <div><h3>Moves</h3> ${pokeMovesResumen}</div>
-                </div>
-            </div>            
-            <div class="bg"></div>
-            `;
-
-
-
-        //btn cerrar
-        const btnClose = document.createElement('a');
-        btnClose.innerHTML = '<i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>';
-        btnClose.classList.add('btn-close');
-        btnClose.addEventListener('click', () => {
-            cerrarModal();
-        })
-
-        //btn next
-        const btnNext = document.createElement('a');
-        btnNext.innerHTML = '<i class="fa fa-chevron-right fa-2x" aria-hidden="true"></i>';
-        btnNext.classList.add('btn-next');
-        btnNext.addEventListener('click', () => {
-            pokeAlertInfo(nextID);
-        })
-
-        //btn prev
-        const btnPrev = document.createElement('a');
-        btnPrev.innerHTML = '<i class="fa fa-chevron-left fa-2x" aria-hidden="true"></i>';
-        btnPrev.classList.add('btn-prev');
-        btnPrev.addEventListener('click', () => {
-            pokeAlertInfo(prevID);
-        })
-
-        infoContainer.setAttribute('id', `poke${pokemon.id}`);
-        infoContainer.classList.add('active');
-        info.prepend(btnClose, btnNext, btnPrev);
-    });
-    
-
-    
-
+            </div> 
+            <div class="info">
+                <div><h3>Abilities</h3> ${pokeAbility}</div>
+                <div><h3>Moves</h3> ${pokeMovesResumen}</div>
+            </div>
+        </div>            
+        <div class="bg"></div>
+        `;
     
 }
+
 const cerrarModal = ()=>{
     infoContainer.classList.remove('active');
     info.innerHTML = '';
