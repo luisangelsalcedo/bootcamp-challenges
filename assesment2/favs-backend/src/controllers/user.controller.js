4ec0e3b7ad0b96357ad88a6c29dbd6b9a9716b21
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2022-04-07 19:28:42 -0500

    installing jsonwebtoken and implementing log in handler

--- assesment2/favs-backend/src/controllers/user.controller.js
+++ assesment2/favs-backend/src/controllers/user.controller.js
@@ -1,7 +1,46 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const login = (req, res) => {};
+/**
+ * * log in with a username and password
+ * @param {Object} req - object http request
+ * @param {String} req.body.email - email sent to validate login
+ * @param {String} req.body.password - password sent to validate login
+ * @return {Object http response} - status 200 return {token} | status 403,500 return {message}
+ */
+export const login = async (req, res) => {
+  const { email, password: pass } = req.body;
+
+  if (!email) return res.status(500).json({ message: "Email is required" });
+  if (!pass) return res.status(500).json({ message: "Password is required" });
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(403).json({ message: "User not found" });
+
+  // compare hash
+  const hashIsCorrect = await bcrypt.compare(pass, user.password);
+
+  if (!hashIsCorrect)
+    return res.status(403).json({ message: "Password is not correct" });
+
+  //JWT
+  const payload = {
+    id: user._id,
+    name: user.name,
+  };
+
+  jwt.sign(
+    payload,
+    process.env.JWT_PASSWORD,
+    {
+      expiresIn: "1d",
+    },
+    (err, token) => {
+      !err && res.status(200).json({ token });
+    }
+  );
+};
 
 /**
  * * Create a new user with a username and password
